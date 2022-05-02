07037b598e5d3e22e3d8b4f3b9beed46aa64b9dc
Hrvoje <htomic4@gmail.com>
2022-05-02 20:43:44 +0100

    commit

--- MushroomLodge/frontend/src/App.js
+++ MushroomLodge/frontend/src/App.js
@@ -1,8 +1,26 @@
+import data from "./data";
+import "./App.css";
 
 function App() {
   return (
     <div>
-       
+      <h1>Fresh mushrooms</h1>
+      <div className="products">
+      {data.products.map(product => (
+        <div className="product-card" key={product.slug}>
+            <a href={"/product/${product.slug}"}>
+            <img src={product.image} alt={product.name} />
+            </a>
+            <div className="product-info">
+            <a href={"/product/${product.slug}"}>
+            <p>{product.name}</p>
+            </a>
+            <p>£{product.price}</p>
+            <button>Add to cart</button>
+            </div>
+          </div>))
+      }     
+      </div>
     </div>
     
   );
---
+++ MushroomLodge/frontend/src/data.jsx
@@ -0,0 +1,33 @@
+const data = {
+    products: [
+        {
+            name: "Pink Oyster Mushrooms",
+            slug: "pink-oyster",
+            category: "Oysters",
+            image: "/images/pink-oyster.JPG",
+            price: 12,
+            countInStock: 10,
+            descritpion: "gourmet mushroom",
+        },
+        {
+            name: "Blue Oyster Mushrooms",
+            slug: "blue-oyster",
+            category: "Oysters",
+            image: "/images/blue-oyster.JPG",
+            price: 12,
+            countInStock: 8,
+            descritpion: "gourmet mushroom",
+        },
+        {
+            name: "Lion's Mane Mushrooms",
+            slug: "lions-mane",
+            category: "Medicinal",
+            image: "/images/lions-mane.JPG",
+            price: 14,
+            countInStock: 10,
+            descritpion: "brain boosting medicinal mushroom",
+        },
+
+    ]
+}
+export default data;
